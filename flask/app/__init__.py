526464c32696bbf399689dd3fb16dbb1a42e82a8
Laryssa Paiva <laryssapaiva@id.uff.br>
2024-06-16 18:44:50 -0300

    add docker files

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -2,33 +2,29 @@ import os
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
 from flask_restx import Api
-from flask_jwt_extended import JWTManager
 from .db_manager import Db_manager
 
 from .routes.hello_routes import hello_ns
 
-from datetime import timedelta
-
 api = Api()
 # db = Db_manager().get_db()
-migrate = Migrate()
-jwt = JWTManager()
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["SQLALCHEMY_DATABASE_URI"]
+    
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{username}:{password}@{host}:{port}/{database}'.format(
+            username=os.environ['RDS_USERNAME'],
+            password=os.environ['RDS_PASSWORD'],
+            host=os.environ['RDS_HOSTNAME'],
+            port=os.environ['RDS_PORT'],
+            database=os.environ['RDS_DB_NAME'],
+        )
+
+    db.init_app(app)
 
     api.init_app(app)
-    # db.init_app(app)
-    # migrate.init_app(app, db)
-
-    jwt.init_app(app)
-    # app.config['JWT_SECRET_KEY'] = os.environ['JWT_SECRET_KEY']
-    # app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=30)
-    # app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(hours=1)
-
     api.add_namespace(hello_ns)
 
     return app   
--- flask/run.py
+++ flask/run.py
@@ -1,7 +1,12 @@
 from flask import Flask
-from app import create_app
+from app import create_app, db
+
+from app.models.user_model import User
 
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
