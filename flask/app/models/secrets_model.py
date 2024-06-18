2313c442b30197ed8cdfe64b9126fb6f6ff4bd28
Laryssa Machado Rabelo de Paiva <laryssa.paiva@green4t.com>
2024-06-18 19:41:57 -0300

    create extensions.py to fix dual circular import

--- flask/app/models/secrets_model.py
+++ flask/app/models/secrets_model.py
@@ -1,14 +1,13 @@
-from app import db
+from app.extensions import db
 
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 class Secrets(db.Model):
-
     id = db.Column(db.Integer, primary_key=True)
     key_id = db.Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
     created_at = db.Column(db.DateTime, nullable=False)
     raw = db.Column(db.String(1000), nullable=False)
 
-    def __str__(self): 
-        return f"id {self.id}, key_id ({self.key_id}), registered on {self.created_at}, raw: {raw}"
+    # def __str__(self): 
+    #     return f"id {self.id}, key_id ({self.key_id}), registered on {self.created_at}, raw: {raw}"
--- flask/app/routes/declare_namespace.py
+++ flask/app/routes/declare_namespace.py
@@ -1,3 +1,5 @@
 from flask_restx import Namespace
 
 hello_ns = Namespace("hello")
+
+v1_ns = Namespace("v1_ns")
