b0605a7653f598b8f59400b07125b01a7d2e633e
Laryssa Machado Rabelo de Paiva <laryssa.paiva@green4t.com>
2024-06-18 19:42:50 -0300

    create get secrets controller

---
+++ flask/app/controllers/get_secrets_controller.py
@@ -0,0 +1,25 @@
+from app.models.secrets_model import Secrets, db
+
+import pandas as pd
+import sqlalchemy
+
+def get_data_db(query):
+    df = pd.read_sql(query.statement, db.engine)
+    results = df.to_dict(orient='records')
+
+    return results
+
+def filter_query():
+    query = Secrets.query.with_entities(
+        Secrets.created_at,
+        Secrets.raw,
+        Secrets.key_id
+    )
+
+    return query
+
+def get_data():
+    query = filter_query()
+    data = get_data_db(query)
+
+    return data
