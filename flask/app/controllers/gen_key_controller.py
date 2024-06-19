65c1553b8ebcedc865cc00889bf7c2365aa4e182
Laryssa Machado Rabelo de Paiva <laryssa.paiva@green4t.com>
2024-06-18 22:04:10 -0300

    add get key

--- flask/app/controllers/gen_key_controller.py
+++ flask/app/controllers/gen_key_controller.py
@@ -2,32 +2,53 @@ from app.models.keys_model import Keys, db
 
 from datetime import datetime
 from hashlib import sha256
+import uuid
 from sqlalchemy import insert
 
-def insert_key_data(email, time):
+def insert_key_data(email, date):
+    id = str(uuid.uuid4())
+    
     key = Keys(
+        id = id,
         email = email,
-        date = time
+        date = date
     )
 
     db.session.add(key)
     db.session.commit()
 
-def get_key_data(token):
-    query = Keys.query.with_entities(Keys.id).all()
+def get_id(email, date):
+    query = Keys.query\
+            .with_entities(Keys.id)\
+            .where(
+                Keys.email == email,
+                Keys.date == date
+            ).all()
+    
+    uuid_value = query[0][0]
+    uuid_string = str(uuid_value)
+
+    return uuid_string
 
 
-def create_token(email, time):
-    token = sha256(email+time.encode('utf-8')).hexdigest()
+def create_token(email, date):
+    input_str = email + date.strftime('%Y-%m-%d %H:%M:%S')
+    token = sha256(input_str.encode('utf-8')).hexdigest()
     
     return token
     
 
 def get_data(email):
-    time = datetime.now()
-    token = create_token(email, time)
+    date = datetime.now()
+    token = create_token(email, date)
     
-    insert_key_data(token, email)
-    get_data(email, time)
+    insert_key_data(email, date)
+    
+    id = get_id(email, date)
 
-    return True
+    response = {
+        "key_id": token,
+        "id": id
+    }
+
+    return response
