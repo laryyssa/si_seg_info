75d390149c7f6fb7458611a093215c854b372d6a
Leonardo Calvet <leonardofmcalvet@gmail.com>
2024-06-26 05:14:57 -0300

    Adição da unicidade para o key e key_ião da unicidade para o key e

--- flask/app/controllers/gen_key_controller.py
+++ flask/app/controllers/gen_key_controller.py
@@ -1,5 +1,4 @@
 from app.models.keys_model import Keys, db
-
 from datetime import datetime
 import uuid
 import secrets
@@ -10,11 +9,15 @@ DEFAULT_TOKEN_LENGTH = 32
 
 def insert_key_data(email, date):
     id = str(uuid.uuid4())
-    
+
+    existing_key = Keys.query.filter_by(id=id).first()
+    if existing_key:
+        raise ValueError("Chave já existe, tente novamente.")
+
     key = Keys(
-        id = id,
-        email = email,
-        date = date
+        id=id,
+        email=email,
+        date=date
     )
 
     db.session.add(key)
@@ -27,12 +30,11 @@ def get_id(email, date):
                 Keys.email == email,
                 Keys.date == date
             ).all()
-    
+
     uuid_value = query[0][0]
     uuid_string = str(uuid_value)
 
     return uuid_string
-
 
 def create_token(email, date):
     email_bytes = email.encode('utf-8')
@@ -40,22 +42,21 @@ def create_token(email, date):
     msg_bytes = email_bytes + date_bytes
 
     sha256_hash = SHA256.new(msg_bytes).digest()
-    
+
     token = base64.urlsafe_b64encode(sha256_hash).decode('utf-8')
-    
+
     while len(token) < DEFAULT_TOKEN_LENGTH:
         random_bytes = secrets.token_bytes(256)
         sha256_hash = SHA256.new(random_bytes).digest()
         token += base64.urlsafe_b64encode(sha256_hash).decode('utf-8')
     return token[:DEFAULT_TOKEN_LENGTH]
-    
 
 def get_data(email):
     date = datetime.now()
     token = create_token(email, date)
-    
+
     insert_key_data(email, date)
-    
+
     id = get_id(email, date)
 
     response = {
--- flask/app/models/secrets_model.py
+++ flask/app/models/secrets_model.py
@@ -1,14 +1,13 @@
 from app.extensions import db
-
-from sqlalchemy.dialects.postgresql import UUID
 from datetime import datetime
+from sqlalchemy.dialects.postgresql import UUID
 import uuid
 
 class Secrets(db.Model):
-    id: int = db.Column(db.Integer, primary_key=True)
-    key_id: uuid.UUID = db.Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
-    created_at: datetime = db.Column(db.DateTime, nullable=False)
-    raw: str = db.Column(db.String(1000), nullable=False)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    key_id = db.Column(UUID(as_uuid=True), nullable=False, default=uuid.uuid4)
+    created_at = db.Column(db.DateTime, nullable=False)
+    raw = db.Column(db.String(1000), nullable=False)
 
-    def __str__(self): 
+    def __str__(self):
         return f"id {self.id}, key_id ({self.key_id}), registered on {self.created_at}, raw: {self.raw}"
